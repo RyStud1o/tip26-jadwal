8b09d8278ef176dbb887359b5a683eb74289045b
RyStud1o <jepryyy6578z@gmail.com>
2026-08-25 22:37:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.onkeydown = function(e) {
   if(e.keyCode == 123) return false; // F12
   if(e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74 || e.keyCode === 67)) return false; // Ctrl+Shift+I/J/C
   if(e.ctrlKey && e.keyCode === 85) return false; // Ctrl+U
-
+};
+  
 // ==========================================
 // 1. INISIALISASI FIREBASE
 // ==========================================
